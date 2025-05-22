30a8cce29aa90176a84d24cf31fa154c0ad8eacd
vbonnard <vbonnard.dev@gmail.com>
2025-05-22 22:22:36 +0200

    norm realloc

--- src/memory/ft_realloc.c
+++ src/memory/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: vbonnard <vbonnard@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 21:37:24 by ezeppa            #+#    #+#             */
-/*   Updated: 2025/05/22 22:22:14 by vbonnard         ###   ########.fr       */
+/*   Updated: 2025/05/22 22:22:34 by vbonnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
 	void	*new_ptr;
+	size_t	copy_size;
 
 	if (!new_size)
 		return (free(ptr), NULL);
@@ -23,8 +24,6 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (ptr);
-	size_t	copy_size;
-
 	if (old_size < new_size)
 		copy_size = old_size;
 	else
