f4412583d4aeb43c1b4b63016b917b0dd3f02e42
vbonnard <vbonnard.dev@gmail.com>
2025-05-22 22:20:44 +0200

    fix realloc

--- src/memory/ft_realloc.c
+++ src/memory/ft_realloc.c
@@ -6,16 +6,15 @@
 /*   By: vbonnard <vbonnard@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 21:37:24 by ezeppa            #+#    #+#             */
-/*   Updated: 2025/02/13 14:05:23 by vbonnard         ###   ########.fr       */
+/*   Updated: 2025/05/22 22:20:36 by vbonnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_realloc(void *ptr, size_t new_size)
+void    *ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
-	void	*new_ptr;
-
+	void    *new_ptr;
 	if (!new_size)
 		return (free(ptr), NULL);
 	if (!ptr)
@@ -23,6 +22,7 @@ void	*ft_realloc(void *ptr, size_t new_size)
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (ptr);
-	ft_memcpy(new_ptr, ptr, new_size);
+	ft_memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
+	free(ptr);
 	return (new_ptr);
 }
